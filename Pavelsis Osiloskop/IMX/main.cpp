4c0a8ee11715af95e125a09970a723150e211c03
Yavuz Selim <yavuzselim2731@gmail.com>
2023-08-17 16:44:27 +0300

    IMX ready files
    These files are compiled using YOCTO SDK to run on IMX8 board

---
+++ Pavelsis Osiloskop/IMX/main.cpp
@@ -0,0 +1,16 @@
+#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.setStyleSheet("#Widget{background-color: #525252;}");
+    w.setWindowTitle("Pavelsis");
+    //w.setFixedSize(1280,720);
+    w.show();
+    return a.exec();
+}
+
+
